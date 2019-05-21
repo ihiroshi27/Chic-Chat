cebef956e9f482c5e079b30dfb973c70cceb2153
ihiroshi27 <ihiroshi27@Hiroshis-MacBook-Pro-15.local>
2019-05-22 00:58:10 +0700

    Add loading on fetch friend

--- view/src/route/search.js
+++ view/src/route/search.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 
 import FriendList from '../component/friendList';
 
+import './search.css';
+
 const API_URL = process.env.REACT_APP_API_URL;
 
 class Search extends React.Component {
@@ -32,24 +34,34 @@ class Search extends React.Component {
 		});
 	}
 	render() {
-		return (
-			<div id="container">
-				<div className="wrapper">
-					<div className="section">
-						<div className="title"><Link to="/"><i className="fas fa-angle-left"></i></Link> Find Friend</div>
-						<div className="subtitle"><i className="far fa-user"></i> Discovered { this.state.friends.length } friends for { new URLSearchParams(this.props.location.search).get("q") }</div>
-						<div className="body">
-							<div className="friend-list">
-							{
-								!this.state.friends_fetched ? null :
-									<FriendList friends={ this.state.friends } refetch={ this.fetchFriend } />
-							}
+		if (!this.state.friends_fetched) {
+			return (
+				<div id="container">
+					<div id="search" className="wrapper">
+						<div className="loading"><div></div><div></div></div>
+					</div>
+				</div>
+			)
+		} else {
+			return (
+				<div id="container">
+					<div id="search" className="wrapper">
+						<div className="section">
+							<div className="title"><Link to="/"><i className="fas fa-angle-left"></i></Link> Find Friend</div>
+							<div className="subtitle"><i className="far fa-user"></i> Discovered { this.state.friends.length } friends for { new URLSearchParams(this.props.location.search).get("q") }</div>
+							<div className="body">
+								<div className="friend-list">
+								{
+									!this.state.friends_fetched ? null :
+										<FriendList friends={ this.state.friends } refetch={ this.fetchFriend } />
+								}
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		)
+			)
+		}
 	}
 }
 
