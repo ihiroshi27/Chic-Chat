16f7aa076137c3041c597d5fda160b7b62ecbe05
ihiroshi27 <ihiroshi27@Hiroshis-MacBook-Pro-15.local>
2019-06-01 12:19:33 +0700

    Prevent null search

--- controller/search.js
+++ controller/search.js
@@ -10,44 +10,48 @@ router.get('/', function(req, res, next) {
 	if (typeof req.headers.authorization === "undefined") {
 		next(new Error('Invalid Token'));
 	} else {
-		token.decode(req.headers.authorization.replace('Bearer ', ''))
-		.then((user) => {
-			let userID = user.id;
-			User.findAll({ 
-				where: {
-					id: { [Op.not]: userID },
-					[Op.or]: [
-						{ name: { [Op.like]: '%' + query + '%' } },
-						{ username: { [Op.like]: '%' + query + '%' } },
-						{ email: { [Op.like]: '%' + query + '%' } },
-						{ mobile: { [Op.like]: '%' + query + '%' } }
-					]
-				}
-			})
-			.then((users) => {
-				let results = users.map(async (user, index) => {
-					let friend = await Friend.findOne({ where: { user_id: userID, friend_id: user.id  } });
-					if (friend) {
-						users[index].friended = "YES"
-					} else {
-						users[index].friended = "NO"
+		if (!query.trim()) {
+			res.json({ friends: [] });
+		} else {
+			token.decode(req.headers.authorization.replace('Bearer ', ''))
+			.then((user) => {
+				let userID = user.id;
+				User.findAll({ 
+					where: {
+						id: { [Op.not]: userID },
+						[Op.or]: [
+							{ name: { [Op.like]: '%' + query + '%' } },
+							{ username: { [Op.like]: '%' + query + '%' } },
+							{ email: { [Op.like]: '%' + query + '%' } },
+							{ mobile: { [Op.like]: '%' + query + '%' } }
+						]
 					}
-				});
-				Promise.all(results).then((friends) => {
-					res.json({
-						friends: users.map((user) => ({
-							id: user.id,
-							name: user.name,
-							username: user.username,
-							profile: user.profile,
-							friended: user.friended
-						}))
-					})
-				});
+				})
+				.then((users) => {
+					let results = users.map(async (user, index) => {
+						let friend = await Friend.findOne({ where: { user_id: userID, friend_id: user.id  } });
+						if (friend) {
+							users[index].friended = "YES"
+						} else {
+							users[index].friended = "NO"
+						}
+					});
+					Promise.all(results).then((friends) => {
+						res.json({
+							friends: users.map((user) => ({
+								id: user.id,
+								name: user.name,
+								username: user.username,
+								profile: user.profile,
+								friended: user.friended
+							}))
+						})
+					});
+				})
+				.catch((err) => next(err));
 			})
 			.catch((err) => next(err));
-		})
-		.catch((err) => next(err));
+		}
 	}
 });
 
